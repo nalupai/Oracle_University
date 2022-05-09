81f1277fb1a7deb7f4fb7c9ad86fee69ac788da7
springwithjava <102017422+springwithjava@users.noreply.github.com>
2022-05-09 10:50:16 +0200

    Exercise 3-1
    Using String Variables

--- Java Explorer/duke.choice/duke/choice/ShopApp.java
+++ Java Explorer/duke.choice/duke/choice/ShopApp.java
@@ -4,8 +4,12 @@ class ShopApp {
     
     public static void main(String[] args) {
 
+        System.out.println("Welcome to Duke Choice Shop");
+
         Customer c1 = new Customer();
+
         c1.name = "Pinky";
-        System.out.println(c1.name);
+
+        System.out.println("Customer is " + c1.name);
     }
 }
