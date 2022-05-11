9c637fb0c1e3e8ee08b7f560e56904f63c0764d5
springwithjava <102017422+springwithjava@users.noreply.github.com>
2022-05-11 14:46:31 +0200

    Exercise 5-1
    Apply Encapsulation

--- Java Explorer/duke.choice/duke/choice/ShopApp.java
+++ Java Explorer/duke.choice/duke/choice/ShopApp.java
@@ -4,43 +4,44 @@ class ShopApp {
     
     public static void main(String[] args) {
 
-        double tax = 0.2, total = 0.0;
+        //double tax = 0.2;
+        double total = 0.0;
 
         System.out.println("Welcome to Duke Choice Shop");
 
         Customer c1 = new Customer();
 
-        c1.name = "Pinky";
-        c1.size = "S";
+        c1.setName("Pinky");
+        c1.setSize("S");
 
-        System.out.println("Customer is " + c1.name);
+        System.out.println("Customer is " + c1.getName());
 
         Clothing item1 = new Clothing();
         Clothing item2 = new Clothing();
 
         Clothing[] items = {item1, item2, new Clothing(), new Clothing()};
 
-        item1.description = "Blue Jacket";
-        item1.price = 20.9;
-        item1.size = "M";
+        item1.setDescription("Blue Jacket");
+        item1.setPrice(20.9);
+        item1.setSize("M");
 
-        item2.description = "Orange T-Shirt";
-        item2.price = 10.5;
-        item2.size = "S";
+        item2.setDescription("Orange T-Shirt");
+        item2.setPrice(10.5);
+        item2.setSize("S");
 
-        items[2].description = "Green Scarf";
-        items[2].price = 5.0;
-        items[2].size = "S";
+        items[2].setDescription("Green Scarf");
+        items[2].setPrice(5.0);
+        items[2].setSize("S");
 
-        items[3].description = "Blue T-Shirt";
-        items[3].price = 10.5;
-        items[3].size = "S";
+        items[3].setDescription("Blue T-Shirt");
+        items[3].setPrice(10.5);
+        items[3].setSize("S");
 
         for (Clothing item : items) {
 
-            if (item.size.equals(c1.size)) {
-                System.out.println(item.description + ", " + item.price + ", " + item.size);
-                total = total + item.price * (1 + tax);
+            if (item.getSize().equals(c1.getSize())) {
+                System.out.println(item.getDescription() + ", " + item.getPrice() + ", " + item.getSize());
+                total = total + item.getPrice();
             }
 
             if (total > 15) {
@@ -48,26 +49,22 @@ class ShopApp {
             }
         }
 
-        //System.out.println(item1.description + ", " + item1.price + ", " + item1.size);
-        //System.out.println(item2.description + ", " + item2.price + ", " + item2.size);
-
-        //total = (item1.price + item2.price * 2) * (1 + tax);
         System.out.println("Total = " + total);
 
         int measurement = 3;
 
         switch (measurement) {
             case 1: case 2: case 3:
-                c1.size = "S";
+                c1.setSize("S");
                 break;
             case 4: case 5: case 6:
-                c1.size = "M";
+                c1.setSize("M");
                 break;
             case 7: case 8: case 9:
-                c1.size = "L";
+                c1.setSize("L");
                 break;
             default:
-                c1.size = "X";
+                c1.setSize("X");
         }
     }
 }
