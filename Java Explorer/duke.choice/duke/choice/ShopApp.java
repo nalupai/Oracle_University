b9aa9d1d44bb436cf449157a316ec98a47604837
springwithjava <102017422+springwithjava@users.noreply.github.com>
2022-05-30 22:43:51 +0200

    Cloud Deployment

--- Java Explorer/duke.choice/duke/choice/ShopApp.java
+++ Java Explorer/duke.choice/duke/choice/ShopApp.java
@@ -1,52 +1,40 @@
 package duke.choice;
 
-import java.util.Arrays;
+import io.helidon.webserver.Routing;
+import io.helidon.webserver.ServerConfiguration;
+import io.helidon.webserver.WebServer;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 class ShopApp {
     
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Duke Choice Shop");
+        System.out.println("Welcome to Duke Choice Shop!");
 
         Customer c1 = new Customer("Pinky", 3);
 
-        System.out.println("Customer is " + c1.getName());
+        System.out.println("Customer is " + c1.getName() + ".");
 
         System.out.println("minimum price = " + Clothing.getMIN_PRICE());
 
         Clothing item1 = new Clothing("Blue Jacket", 20.9, "M");
         Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
 
-        Clothing[] items = {item1, item2, new Clothing("Green Scarf", 5.0, "S"), new Clothing("Blue T-Shirt", 10.5, "S")};
-
-        c1.addItems(items);
-
-        Arrays.sort(c1.getItems());
-
-        for (Clothing item : items) {
-            //System.out.println(item.getDescription() + ", " + item.getPrice() + ", " + item.getSize());
-            System.out.println(item);
-        }
+        Clothing[] items = {item1, item2, new Clothing("Green Scarf", 5.0, "S"),
+            new Clothing("Blue T-Shirt", 10.5, "S")};
         
-        System.out.println("Total = " + c1.getTotalClothingCost(items));
-
-        double sum = 0;
-        double average = 0;
-        int counter = 0;
-
-        for (Clothing item : c1.getItems()) {
-            if (item.getSize().equals("L")) {
-                sum = sum + item.getPrice();
-                counter++;
-            }
-        }
-
         try {
-            average = sum / counter;
-            double zeroCheck = 0 / (int)average;
-            System.out.println("average price = " + average);
-        } catch (ArithmeticException e) {
-            System.out.println("calling an exception");
+            ItemList list = new ItemList(items);
+            Routing routing = Routing.builder()
+                    .get("/items", list).build();
+            ServerConfiguration config = ServerConfiguration.builder()
+                    .bindAddress(InetAddress.getLocalHost())
+                    .port(8888).build();
+            WebServer webserver = WebServer.create(config, routing);
+            webserver.start();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
         }
     }
 }
